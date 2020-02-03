378abd51a71c1cbcaef252e56022eeaa1556e231
Łukasz Jędrusik <ljedrusik84@gmail.com>
2020-02-03 19:42:05 +0100

    dotnet test passed

--- aspcore-k8s.Tests/UnitTest1.cs
+++ aspcore-k8s.Tests/UnitTest1.cs
@@ -12,7 +12,7 @@ namespace Tests
         [Test]
         public void Test1()
         {
-            var actual = "failed";
+            var actual = "passed";
 
             var expected = "passed";
 
